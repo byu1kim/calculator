5397f6e70cc7db62b037d781e9cbd79108ba367f
Byul Kim <75004246+byu1kim@users.noreply.github.com>
2023-09-25 23:08:55 -0700

    comment

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 
 function Calculator() {
   const [input, setInput] = useState("");
-  const [result, setResult] = useState("결과:");
-  const [error, setError] = useState("에러:");
+  const [result, setResult] = useState("결과:"); // 걍 empty string으로 init해도 됨
+  const [error, setError] = useState("에러:"); // 걍 empty string으로 init해도 됨
   const [history, setHistory] = useState([]);
 
   const calculate = () => {
+    
     const regex = /([-+*/])|\d+/g;
     const tokens = input.match(regex);
 
@@ -15,8 +16,9 @@ function Calculator() {
       setResult("결과:");
       return;
     }
-
+    
     let calculationResult = parseFloat(tokens[0]);
+    
     for (let i = 1; i < tokens.length; i += 2) {
       const operator = tokens[i];
       const operand = parseFloat(tokens[i + 1]);
@@ -29,16 +31,16 @@ function Calculator() {
 
       switch (operator) {
         case "+":
-          calculationResult += operand;
+          calculationResult += operand; // setResult(result+=operand) 이런식으로 하면 calculationResult 따로 declare안해도 됨
           break;
         case "-":
-          calculationResult -= operand;
+          calculationResult -= operand; //마찬가지
           break;
         case "*":
-          calculationResult *= operand;
+          calculationResult *= operand; //마찬가지
           break;
         case "/":
-          calculationResult /= operand;
+          calculationResult /= operand; //마찬가지
           break;
         default:
           setError("에러: 유효하지 않은 계산식입니다.");
@@ -48,8 +50,8 @@ function Calculator() {
     }
 
     setError("에러:");
-    setResult(`결과: ${calculationResult}`);
-    setHistory([...history, `${input} = ${calculationResult}`]);
+    setResult(`결과: ${calculationResult}`); // setResult(variable)이렇게 하고 html에서 메세지 띄우는게 나음 html쪽 ㄱㄱ
+    setHistory([...history, `${input} = ${calculationResult}`]); // 이것보단 오브젝트로 해도 괜찮을거 같음 like useState({input: ..., result: ...,})
   };
 
   return (
@@ -62,7 +64,7 @@ function Calculator() {
         onChange={(e) => setInput(e.target.value)}
       />
       <button onClick={calculate}>계산하기</button>
-      <p>{result}</p>
+      <p>{result ? ("결과: " + result) : ("에러: " + error) }</p>
       <p>{error}</p>
       <p>history</p>
       <ul>
